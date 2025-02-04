2a26ac5a7dd2a4f3a8e15a9e06e9273d643d5b48
Mohammed-Khaledx <mohammedkhaledx1@gmail.com>
2025-02-04 12:57:20 +0200

    set up profile routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,15 +3,16 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AdminComponent } from './auth/admin/admin.component';
 import { authGuard } from './services/auth.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-
   {
     path: 'admin',
     component: AdminComponent,
     canActivate: [authGuard],
   },
   { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
 ];
